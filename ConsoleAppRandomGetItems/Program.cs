2acffc00527cad7d63c3717977a5dcb794fa27f8
Renato Groffe <renato.groffe@yahoo.com.br>
2023-09-15 19:06:04 -0300

    Commit inicial
    Commit inicial

---
+++ ConsoleAppRandomGetItems/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.InteropServices;
+using System.Text.Json;
+
+Console.WriteLine("***** Testes com .NET 8 | Metodo Random.Shared.GetItems *****");
+Console.WriteLine($"Versao do .NET em uso: {RuntimeInformation
+    .FrameworkDescription} - Ambiente: {Environment.MachineName} - Kernel: {Environment
+    .OSVersion.VersionString}");
+
+Console.WriteLine();
+Console.WriteLine("** Testes com string[]");
+var sugestoesCidades = new string[]
+{
+    "Sao Paulo", "Nova York", "Roma", "Cairo", "Paris", "Toquio"
+};
+Console.WriteLine($"{nameof(sugestoesCidades)} = " +
+    JsonSerializer.Serialize(sugestoesCidades));
+for (int i = 1; i <= 3; i++)
+{
+    var escolhas = Random.Shared.GetItems(sugestoesCidades, i);
+    Console.WriteLine($"{nameof(sugestoesCidades)} - GetItems {i}/3 = " +
+        JsonSerializer.Serialize(escolhas));
+}
+
+Console.WriteLine();
+Console.WriteLine("** Testes com ReadOnlySpan<string>");
+var sugestoesPratos = new ReadOnlySpan<string>(new string[]
+{
+    "Pizza", "Spaghetti", "Costela bovina", "Sopa", "Bacon", "Esfiha", "Feijoada"
+});
+Console.WriteLine($"{nameof(sugestoesPratos)} - valor original = " +
+    JsonSerializer.Serialize(sugestoesPratos.ToArray()));
+for (int i = 1; i <= 3; i++)
+{
+    var escolhas = Random.Shared.GetItems(sugestoesPratos, i);
+    Console.WriteLine($"{nameof(sugestoesPratos)} - GetItems {i}/3 = " +
+        JsonSerializer.Serialize(escolhas));
+}
---
+++ ConsoleAppRandomShuffle/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.InteropServices;
+using System.Text.Json;
+
+Console.WriteLine("***** Testes com .NET 8 | Metodo Random.Shared.Shuffle *****");
+Console.WriteLine($"Versao do .NET em uso: {RuntimeInformation
+    .FrameworkDescription} - Ambiente: {Environment.MachineName} - Kernel: {Environment
+    .OSVersion.VersionString}");
+
+Console.WriteLine();
+Console.WriteLine("** Testes com string[]");
+var sugestoesCidades = new string[]
+{
+    "Sao Paulo", "Nova York", "Roma", "Cairo", "Paris", "Toquio"
+};
+Console.WriteLine($"{nameof(sugestoesCidades)} - valor original = " +
+    JsonSerializer.Serialize(sugestoesCidades));
+for (int i = 1; i <= 3; i++)
+{
+    Random.Shared.Shuffle(sugestoesCidades);
+    Console.WriteLine($"{nameof(sugestoesCidades)} - Shuffle {i}/3 = " +
+        JsonSerializer.Serialize(sugestoesCidades));
+}
+
+Console.WriteLine();
+Console.WriteLine("** Testes com Span<string>");
+var sugestoesPratos = new Span<string>(new string[]
+{
+    "Pizza", "Spaghetti", "Costela bovina", "Sopa", "Bacon", "Esfiha", "Feijoada"
+});
+Console.WriteLine($"{nameof(sugestoesPratos)} - valor original = " +
+    JsonSerializer.Serialize(sugestoesPratos.ToArray()));
+for (int i = 1; i <= 3; i++)
+{
+    Random.Shared.Shuffle(sugestoesPratos);
+    Console.WriteLine($"{nameof(sugestoesPratos)} - Shuffle {i}/3 = " +
+        JsonSerializer.Serialize(sugestoesPratos.ToArray()));
+}
